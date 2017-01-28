ea5749e01a872448ff3d4ec12b1cf93b2b323e51
Moisés Guimarães <moises@wolfssl.com>
2017-01-28 10:18:03 -0300

    inclui cursos no json

--- csv_to_json.py
+++ csv_to_json.py
@@ -30,6 +30,7 @@ def csv_to_json(csv_file):
 
     return {"turma" : turma, "horarios" : horarios}
 
+cursos = []
 turmas = []
 
 turmas.append(csv_to_json(open("data/csv/Turmas-IN1.csv")))
@@ -37,14 +38,26 @@ turmas.append(csv_to_json(open("data/csv/Turmas-IN2.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-IN3.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-IN4.csv")))
 
+cursos.append({"curso" : "Informática", "turmas" : turmas})
+turmas = []
+
 turmas.append(csv_to_json(open("data/csv/Turmas-ED1.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-ED2.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-ED3.csv")))
+
+cursos.append({"curso" : "Edificações", "turmas" : turmas})
+turmas = []
+
 turmas.append(csv_to_json(open("data/csv/Turmas-CN1.csv")))
+
+cursos.append({"curso" : "Contabilidade", "turmas" : turmas})
+turmas = []
 
 turmas.append(csv_to_json(open("data/csv/Turmas-GC1.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-GC2.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-GC3.csv")))
 turmas.append(csv_to_json(open("data/csv/Turmas-GC4.csv")))
 
-open("data/horarios.json", "w").write(json.dumps({"turmas" : turmas}))
+cursos.append({"curso" : "Gestão Comercial", "turmas" : turmas})
+
+open("data/horarios.json", "w").write(json.dumps({"cursos" : cursos}))
